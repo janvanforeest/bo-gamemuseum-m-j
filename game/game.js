2043f4e5ce36d4aa5f58295c01606bfb224d961b
Rien van Duivenbode <r.v.duijvenbode@ziggo.nl>
2023-01-13 14:44:35 +0100

    eerstegamecommit

---
+++ game/game.js
@@ -0,0 +1,10 @@
+
+
+let canvas = document.getElementById("canvas");
+let g = canvas.getContext("2d")
+
+g.fillstyle = "#2e6ede"; 
+g.fillrect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+g.fillstyle = "#leca07";
+g.fillstyle(0, 0, 10, 10);
