7e21b6db8796defb670be5ebfa9f30820b77aca9
Rien van Duivenbode <r.v.duijvenbode@ziggo.nl>
2023-01-13 15:49:18 +0100

    tweedegamcommit

--- game/game.js
+++ game/game.js
@@ -3,8 +3,8 @@
 let canvas = document.getElementById("canvas");
 let g = canvas.getContext("2d")
 
-g.fillstyle = "#2e6ede"; 
-g.fillrect(0, 0, canvas.clientWidth, canvas.clientHeight);
+g.fillStyle = "#2e6ede"; 
+g.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
 
-g.fillstyle = "#leca07";
-g.fillstyle(0, 0, 10, 10);
+g.fillStyle = "#leca07";
+g.fillRect(0, 0, 10, 10);
