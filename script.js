31896a3bf41a8922504d0b7ab7a4145f77982774
jan van foreest <30220@ma-web.nl>
2023-02-05 23:24:26 +0100

    laatstecommitboopdrachtperiode2

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const mySlider = document.getElementById('sliderButton')
+const myFoto = document.getElementById('myFoto1','myFoto2','myFoto3')
 
 let slider [
-    
+    "img/c870x524.jpg",
+    "img/WhatsApp_Image_2022-12-01_at_12.33.56_1.jpeg",
+    "img/c45d048c2b2af3fc91704d005422dfb8.jpg"
+]   
 
+function shuffle(index,engels)
+myFoto1.m
 
-
-
-
-]
